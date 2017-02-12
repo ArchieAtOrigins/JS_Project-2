fd35a5b59d3f4947aea12d24c5ce443c9f4184aa
ArchieAtOrigins <archie@originscreative.com>
2017-02-11 16:12:36 -0800

    Created showArray. Working on matching user choice to the appropriate array

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
+// sort the names of the character alphabetically.
+// Allow user to select show from dropdown list and 
+// create a list of the main characters in the show selected.
+// Allow user to click on character to see details of that character
 
-const bbArray = ['Walter', 'Jessie', 'Hank', 'Skyler', 'Gus', 'Tuco', 'Pete', 'Badger', 'Flynn', 'Mike', 'Marie', 'Saul', 'Jane', 'Lydia', 'Todd', 'Steve', 'Ted', 'Huell', 'Combo', 'Jack'];
+const bbArray = ['Walter', 'Jessie', 'Hank', 'Skyler', 'Gus', 'Tuco', 'Skinny Pete', 'Badger', 'Flynn', 'Mike', 'Marie', 'Saul', 'Jane', 'Lydia', 'Todd', 'Steve', 'Ted', 'Huell', 'Combo', 'Jack'];
 
 const wdArray = ['Rick', 'Daryl', 'Glenn', 'Maggie', 'Carl', 'Carol', 'Michonne', 'Lori', 'Morgan', 'Tyreese', 'Sasha', 'Beth', 'Tara', 'Hershel'];
 
-const ffArray = ['Malcolm', 'Zoe', 'Hoban', 'Inara', 'Jayne', 'Kaylee', 'Simon', 'River', 'Sheperd', 'Saffron', 'Badger', 'Patience', 'Tara';
+const ffArray = ['Malcolm', 'Zoe', 'Hoban', 'Inara', 'Jayne', 'Kaylee', 'Simon', 'River', 'Sheperd', 'Saffron', 'Badger', 'Patience', 'Tara'];
 
-const wdArray = ['Piper', 'Alex', 'Sophia', 'Stella', 'Suzanne', 'Nicky', 'Doggett', 'Red', 'Poussey', 'Diaz', 'Jackie', 'Lorna', 'Sam', 'Alieda', 'Caputo', 'Norma', 'Gloria', 'Boo', 'Jane', 'Wanda', 'Piscatella', 'Hayes'];
+const obArray = ['Piper', 'Alex', 'Sophia', 'Stella', 'Crazy Eyes', 'Nicky', 'Doggett', 'Red', 'Poussey', 'Diaz', 'Jackie', 'Lorna', 'Sam', 'Alieda', 'Caputo', 'Norma', 'Gloria', 'Big Boo', 'Jane', 'Wanda', 'Piscatella', 'Hayes'];
 
+const showsArray = ['bbArray', 'wdArray', 'ffArray', 'obArray'];
 
+// captures select box value
+const showDropdown = document.getElementById('showName').value; // This is a string
+
+// sort array alphabetically
+const nameSort = function(array) {
+	const arraySorted = array.sort();
+	// return arraySorted;
+}
+console.log(nameSort(obArray));
+
+// creates a numbered list from an array of cast members names
 const castList = function(array) {
 	const charList = document.getElementById('charList');
 
@@ -18,8 +34,12 @@ const castList = function(array) {
 	}
 	return newLi;
 }
-castList(wdArray);
+console.log(castList(obArray)) // Castlist seems to be working fine
 
-
-
+// loop over arrays and look for match to user selected show
+for (var i=0, max=showsArray.length; i<max; i++) {
+	if (showDropdown === showsArray[i]) {
+		castList(showsArray[i]);
+	}
+}
 
